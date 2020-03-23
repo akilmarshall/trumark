ca4ff261571abd9c7895b6bdc7ee66bb9205d10a
Akil Marshall <akilmarshall@gmail.com>
2020-03-22 18:51:14 -1000

    reorganized database scripts

---
+++ database/populate_database.py
@@ -0,0 +1,46 @@
+'''
+This script populates the database with all magic card printings from mtgjson.com
+'''
+from os.path import isfile
+import json
+import requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DB_PATH = 'sqlite:///trumark.db'
+engine = create_engine(DB_PATH, echo=True)
+Session = sessionmaker(bind=engine)
+
+
+def get_all_cards(source_url, file_name):
+    '''
+    function to download a json file located at source_url to a localfile, file_name
+    '''
+    response = requests.get(source_url)
+    all_cards = json.loads(response.text)
+    with open(file_name, 'w') as file:
+        json.dump(all_cards, file, indent=1)
+
+
+def populate_format():
+    '''
+    certain information about formats is not stored in the json card data.
+    min_deck_size, max_deck, and copies_allowed are specified manually by
+    this function
+    '''
+    session = Session()
+
+
+if __name__ == '__main__':
+    all_printings = 'https://www.mtgjson.com/files/AllPrintings.json'
+    file = 'AllPrintings.json'
+
+    if not isfile(file):
+        print(f'{file} not found, downloading from {all_printings}')
+        print('This will take a few minutes.')
+        get_all_cards(all_printings, file)
+
+    populate_format()  # populate the FORMAT table
+
+
