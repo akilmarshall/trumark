3f8d7f3203d36fe7fa6f5c26e20e8b90e2c66561
Akil Marshall <akilmarshall@gmail.com>
2020-03-23 02:41:57 -1000

    now populates all the tables in the database excluding IS_ALLOWED

--- database/populate_database.py
+++ database/populate_database.py
@@ -4,7 +4,7 @@ This script populates the database with all magic card printings from mtgjson.co
 from os.path import isfile
 import json
 import requests
-from create_db import Format, Set
+from create_db import Format, Set, Contains, Limitation, Color, Color_cost, Card, Type, Subtype, Supertype, Color_identity, Is_allowed
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -134,23 +134,175 @@ def populate_format():
     session.commit()
 
 
+def populate_is_allowed():
+    '''
+    formats are distinguished by the sets they allow.
+    this function populates those relationships in the database
+    '''
+    session = Session()
+
+    def create_is_allowed_list(format_name, allowed_sets):
+        return [Is_allowed(set_code=c, format_name=format_name) for c in allowed_sets]
+
+    standard_sets = ['GRN', 'RNA', 'WAR', 'M20', 'ELD', 'THB', 'IKO', 'M21']
+    for s in create_is_allowed_list('standard', standard_sets):
+        session.merge(s)
+
+    for s in create_is_allowed_list('brawl', standard_sets):
+        session.merge(s)
+
+
+    )
+    '''
+
+
+
+
 def populate_rest(file='AllPrintings.json'):
     '''
     All tables except FORMAT is populated by the information in file
     file must be the json from mtgjson.com/file/allprintings
+
+    Sets are the top level object in this json file
+    This function iterates over the sets and first creates a corresponding entity
+    in the SET table and then creates related entities in the all other tables
+    of the database (excluding FORMAT and IS_ALLOWED which is populated by the populate_format function).
     '''
     session = Session()
 
     with open(file) as f:
         card_sets = json.load(f)
         for s in card_sets:
-            temp_set = Set(
-                set_code=card_sets[s]['code'],
-                set_name=card_sets[s]['name'],
-                release_date=card_sets[s]['releaseDate'])
-            session.add(temp_set)
+            set_code = card_sets[s].get('code')  # will need this again a foreign key
+
+            # A Set entity to be added to the SET table
+            set_entity = Set(
+                set_code=set_code,
+                set_name=card_sets[s].get('name'),
+                release_date=card_sets[s].get('releaseDate'),
+                set_type=card_sets[s].get('type'))
+
+            session.add(set_entity)
+
+            for card in card_sets[s]['cards']:
+                card_name = card.get('name')  # will need this again as a foreign key
+
+                # A Card entity to be added to the CARD table
+                card_entity = Card(
+                    card_name=card_name,
+                    text=card.get('text'),
+                    power=card.get('power'),
+                    toughness=card.get('toughness'),
+                    loyalty=card.get('loyalty'))
+
+                session.merge(card_entity)  # merge updates a duplicate entry if it exists
+
+                # A Contains entity to be added to the CONTAINS table
+                contains_entity = Contains(
+                    set_code=set_code,
+                    card_name=card_name,
+                    rarity=card.get('rarity'))
+
+                session.merge(contains_entity)
+
+                for format_ in card['legalities']:
+                    legality = card.get('legalities')[format_]
+                    limitation_type = str()
+                    if legality == 'Legal':
+                        limitation_type = 'none'
+                    else:
+                        limitation_type = legality.lower()
+
+                    # A Limitation entity to be added to the LIMITATION table
+                    limitation_entity = Limitation(
+                        format_name=format_,
+                        card_name=card_name,
+                        limitation_type=limitation_type)
+
+                    session.merge(limitation_entity)
+
+                # dictionary to map the color letters to their corresponding word
+                color_format = {'U': 'blue', 'R': 'red', 'G': 'green', 'B': 'black', 'W': 'white'}
+                # Color entity to be added to the Color table
+                if card['colors'] == []:
+                    color_entity = Color(
+                        card_name=card_name,
+                        color='colorless')
+                else:
+                    for color in card['colors']:
+                        color_entity = Color(
+                            card_name=card_name,
+                            color=color_format[color])
+
+                session.merge(color_entity)
+
+                def format_cost_string(manacost):
+                    if manacost:
+                        return manacost.replace('{', '').replace('}', '')
+                    else:
+                        return ''
+                # Color_cost entity to be added to the Color_cost table
+                cost_string = card.get('manaCost')
+                color_cost_entity = Color_cost(
+                    card_name=card_name,
+                    cost_string=format_cost_string(cost_string))
+
+                session.merge(color_cost_entity)
+
+                supertype = card.get('supertype')
+                if supertype:
+                    for s_type in supertype:
+                        # Supertype entity to be added to the Supertype table
+                        supertype_entity = Supertype(
+                            card_name=card_name,
+                            supertype=s_type)
+
+                        session.merge(supertype_entity)
+
+                subtype = card.get('subtype')
+                if subtype:
+                    for s_type in subtype:
+                        # Subtype entity to be added to the Subtype table
+                        subtype_entity = Subtype(
+                            card_name=card_name,
+                            subtype=s_type)
+
+                        session.merge(subtype_entity)
+
+                types = card.get('types')
+                type_ = set(types)
+                if supertype:
+                    type_.difference_update(supertype)
+
+                if subtype:
+                    type_.difference_update(subtype)
+
+                # Type entity to be added to the Type table
+                for t in type_:
+                    type_entity = Type(
+                        card_name=card_name,
+                        type_=t)
+
+                    session.merge(type_entity)
+
+                color_id = dict()  # dict to keep track of the color identity flags
+                for color in ['R', 'G', 'U', 'B', 'W']:
+                    color_id[color] = False  # default alignment is false
+                    if color in card['colorIdentity']:
+                        color_id[color] = True
+
+                # Color_identity entity to be added to the Color_identity table
+                color_identity_entity = Color_identity(
+                    card_name=card_name,
+                    red=color_id['R'],
+                    blue=color_id['U'],
+                    green=color_id['G'],
+                    black=color_id['B'],
+                    white=color_id['W'])
+
+                session.merge(color_identity_entity)
+
             session.commit()
-            quit()
 
 
 
