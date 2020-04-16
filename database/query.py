9ab21852fa8607f3708bf849af7b8fe3cce54fe3
John Kuroda <john1@ohe.local>
2020-04-15 17:40:36 -1000

    joins

--- database/query.py
+++ database/query.py
@@ -11,12 +11,20 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
+# generalize and turn these into functions
 for card in session.query(Card).filter(Card.card_name == 'Fireball'):
     print(card)
-
+print('\n\n\n\n')
 for card in session.query(Card).filter(Card.text.like('%ball%')):
     print(card)
-
+print('\n\n\n\n')
+card_join_limitation = session.query(Card).join(Limitation)
+card_join_limitation2 = session.query(Limitation).join(Card)
+for card in card_join_limitation.filter(Limitation.limitation_type == 'restricted'):
+    print(card)
+print('\n\n\n\n')
+for limit in card_join_limitation2.filter(Limitation.limitation_type == 'restricted'):
+    print(limit)
 
 
 
