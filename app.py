66a27ba3e1b92cb10e5ec75023bd4d791ad19def
akil <akil@testarch.localdomain>
2020-04-30 01:56:55 -1000

    added query box to the webpage and exposes its user entered content to python

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sometimes I wander in the mid morning.'
+# app.config['SECRET_KEY'] = 'sometimes I wander in the mid morning.'
 Bootstrap(app)
 
+def decode(user_query:str):
+    # function from a user_query to function calls that pull data from the database 
+    pass
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def root():
-    return render_template('index.html')
+    if request.method == 'POST':
+        # will print the user input to terminal
+        print(request.form['query'])
+
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
