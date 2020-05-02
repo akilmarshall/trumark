e625160663d4562ab64b19b90286101fed4eaf56
John Kuroda <john1@ohe.lan>
2020-05-02 12:52:02 -1000

    query command changes, removed extra table joins that caused too much output

--- flask/app.py
+++ flask/app.py
@@ -29,6 +29,7 @@ def getQuery():
     domain, case = clauses[0].split(':')
     domain = domain.lower().strip()
     case = case.lower().strip()
+    print(f'[{domain}: {case}]')
 
     results = db.session
     # create base case, for first clause.
@@ -42,23 +43,9 @@ def getQuery():
         case = case.lower().strip()
         results = append_query(results, domain, case)
 
-    #results = [x[0].card_name for x in results]
-    results = [pretty_formatter(x) for x in results]
     # return the results to html
     return render_template('results.html', results=results)
 
-def pretty_formatter(tup):
-    '''return nicely formatted strings for end users'''
-    ret_val = []
-    for entity in tup:
-        if entity.__tablename__ == 'CARD': 
-            ret_val.append(f'[{entity.card_name}]')
-            ret_val.append(f'[{entity.text}]')
-        if entity.__tablename__ == 'COLOR_COST':
-            ret_val.append(f'[{entity.cost_string}]')
-        if entity.__tablename__ == 'Subtype':
-            ret_val.append(f'[{entity.subtype}]')
-    return ' '.join(ret_val) + ';'
 
 def single_query(session, domain, case):
     '''process the query and return the results of the generated sql statement.
@@ -71,25 +58,22 @@ def single_query(session, domain, case):
     that can be used as a base for further table joins or filters.'''
 
     # the default tables that we display from, we are always interested in casting cost.
-    # table info printed to webpage is based on tables queried.
-    tables = [Card, Color_cost]
     if domain == '!':  # maybe make this 'name'
         # Search for keyword in CARD JOIN COLOR_COST
         # Wanted to see casting cost with card, always.
-        results = session.query(*tables).join(Color_cost).filter(Card.card_name.like(f'%{case}%'))
+        results = session.query(Card.card_name).add_column(Color_cost.cost_string).join(Color_cost).filter(Card.card_name.like(f'%{case}%'))
 
     # Search for keyword in CARD text, e.g., 'o:island walk'
     elif domain == 'o': 
-        results = session.query(*tables).filter(Card.text.like(f'%{case}%'))
+        results = session.query(Card.text).add_column(Card.card_name).filter(Card.text.like(f'%{case}%'))
     # search for a specific mana cost, e.g., 'mana:{U}{U}'
     elif domain == 'mana': 
         # cost string must include brackets, e.g., {1}{G}{G}. upper() needed because all queries are lower().
-        results = session.query(*tables).join(Card).filter(Color_cost.cost_string == case.upper())
+        results = session.query(Color_cost.cost_string).add_column(Color_cost.card_name).filter(Color_cost.cost_string == case.upper())
     
     # search for a subtype, e.g., 'sub:Merfolk'
     elif domain == 'sub':
-        # NOTE: .add_entity(tablename) adds printing info for user
-        results = session.query(*tables).add_entity(Subtype).join(Subtype).filter(Subtype.subtype.like(f'%{case}%'))
+        results = session.query(Subtype.subtype).add_column(Subtype.card_name).filter(Subtype.subtype.like(f'%{case}%'))
 
     # search for cards that have one or more colors, e.g., 'c:rg' for red AND green cards
     elif domain == 'c': # maybe make this 'color'
@@ -99,14 +83,14 @@ def single_query(session, domain, case):
         for col in colors:
             if col in col_d.keys():
                 col_d[col] = 1
-        results = session.query(*tables).add_entity(Color_identity).join(Color_identity).filter(Color_identity.black == col_d['b'])
+        results = session.query(Color_identity).filter(Color_identity.black == col_d['b'])
         results = results.filter(Color_identity.blue == col_d['u'])
         results = results.filter(Color_identity.green == col_d['g'])
         results = results.filter(Color_identity.white == col_d['w'])
         results = results.filter(Color_identity.red == col_d['r'])
     
 
-    
+
     # probably add elif statements at least for type, limitation, format, and set
 
 
@@ -153,12 +137,13 @@ def append_query(results, domain, case):
                 results = results.join(Color_cost).filter(Color_cost.cost_string == case.upper())
         
         elif domain == 'sub':
+            print('in sub')
             # With two tables there are four conditions we need to check for before joining.
             if 'CARD' in sql_statement and 'SUBTYPE' in sql_statement:
                 results = results.add_entity(Subtype).filter(Subtype.subtype.like(f'%{case}%'))
             if 'CARD' in sql_statement and 'SUBTYPE' not in sql_statement:
                 results = results.add_entity(Subtype).join(Subtype).filter(Subtype.subtype.like(f'%{case}%'))
-            if 'CARD' not in sql_statemençt and 'SUBTYPE' in sql_statement:
+            if 'CARD' not in sql_statement and 'SUBTYPE' in sql_statement:
                 results = results.add_entity(Subtype).join(Card).filter(Subtype.subtype.like(f'%{case}%'))
             if 'CARD' not in sql_statement and 'SUBTYPE' not in sql_statement:
                 results = results.add_entity(Subtype).join(Card).join(Subtype).filter(Subtype.subtype.like(f'%{case}%'))
